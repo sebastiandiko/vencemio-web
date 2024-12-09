d7d341fc546df4b780f12413dbe448eca6bde468
Zalmatt <matizalazar2001@gmail.com>
2024-12-09 09:04:22 -0300

    Modify-Map mas limpio de puntos de interes

--- vencemio-web/src/pages/userPages/MapScreen.js
+++ vencemio-web/src/pages/userPages/MapScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { GoogleMap, LoadScript, Marker, InfoWindow } from "@react-google-maps/api";
+import { useNavigate } from "react-router-dom"; // Importa useNavigate para redirigir
 import axios from "axios";
 
 const containerStyle = {
@@ -15,7 +16,9 @@ const MapScreen = () => {
   const [currentLocation, setCurrentLocation] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [mapLoaded, setMapLoaded] = useState(false); // Estado para verificar si Google Maps se ha cargado
+  const [mapLoaded, setMapLoaded] = useState(false);
+
+  const navigate = useNavigate(); // Hook para redirigir
 
   const getSupermarketIcon = (supermarketName) => {
     const normalizeName = (name) =>
@@ -32,7 +35,7 @@ const MapScreen = () => {
       lareina: "/assets/logo_lareina.png",
     };
 
-    return icons[normalizeName(supermarketName)] || "/assets/default-icon.png";
+    return icons[normalizeName(supermarketName)] || null; // Si no tiene icono, devolver null
   };
 
   useEffect(() => {
@@ -82,85 +85,170 @@ const MapScreen = () => {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <LoadScript
-      googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
-      onLoad={() => setMapLoaded(true)} // Establece el estado como cargado
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={currentLocation || defaultCenter}
-        zoom={currentLocation ? 16 : 14}
-        onLoad={() => setMapLoaded(true)} // Marca que el mapa está cargado
+    <div style={{ position: "relative" }}> {/* Contenedor para posicionar el botón */}
+      {/* Botón para volver */}
+      <button
+        style={{
+          position: "absolute",
+          top: "100px",
+          left: "10px",
+          zIndex: 1000,
+          padding: "10px 15px",
+          backgroundColor: "#1c802d",
+          color: "#fff",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+          fontSize: "16px",
+          fontWeight: "bold",
+          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)",
+        }}
+        onClick={() => navigate("/user-home")} // Navega a /user-home
       >
-        {mapLoaded && ( // Solo ejecuta este código si el mapa está completamente cargado
-          <>
-            {currentLocation && (
-              <Marker
-                position={currentLocation}
-                icon={{
-                  url: "https://maps.gstatic.com/mapfiles/ms2/micons/man.png",
-                  scaledSize: new window.google.maps.Size(40, 40),
-                }}
-              />
-            )}
+        Volver
+      </button>
 
-            {locations.map((location) => (
-              <Marker
-                key={location.id}
-                position={{ lat: location.lat, lng: location.lng }}
-                icon={{
-                  url: getSupermarketIcon(location.name),
-                  scaledSize: new window.google.maps.Size(40, 40),
-                }}
-                onClick={() => setSelected(location)}
-              />
-            ))}
-
-            {selected && (
-              <InfoWindow
-                position={{ lat: selected.lat, lng: selected.lng }}
-                onCloseClick={() => setSelected(null)}
-              >
-                <div
-                  style={{
-                    fontFamily: "Arial, sans-serif",
-                    padding: "10px",
-                    textAlign: "center",
-                    backgroundColor: "#fff8e1",
-                    borderRadius: "10px",
-                    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.2)",
-                    maxWidth: "250px",
+      {/* Mapa */}
+      <LoadScript
+        googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
+        onLoad={() => setMapLoaded(true)}
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={currentLocation || defaultCenter}
+          zoom={currentLocation ? 16 : 14}
+          onLoad={() => setMapLoaded(true)}
+          options={{
+            styles: [
+              {
+                featureType: "all",
+                elementType: "geometry",
+                stylers: [
+                  { visibility: "simplified" }
+                ],
+              },
+              {
+                featureType: "poi",
+                elementType: "labels.icon",
+                stylers: [{ visibility: "off" }],  // Desactivar iconos de puntos de interés
+              },
+              {
+                featureType: "road",
+                elementType: "labels.icon",
+                stylers: [{ visibility: "off" }],  // Desactivar iconos de carreteras
+              },
+              {
+                featureType: "transit",
+                elementType: "labels.icon",
+                stylers: [{ visibility: "off" }],  // Desactivar iconos de transporte
+              },
+              {
+                featureType: "road",
+                elementType: "geometry",
+                stylers: [
+                  { visibility: "on" },    // Mantener las carreteras visibles
+                ],
+              },
+              {
+                featureType: "administrative",
+                elementType: "geometry",
+                stylers: [
+                  { visibility: "on" },    // Mantener las fronteras administrativas
+                ],
+              },
+              {
+                featureType: "landscape",
+                elementType: "geometry",
+                stylers: [
+                  { color: "#f1f1f1" },   // Color de las áreas de paisaje
+                  { visibility: "on" },    // Mantener paisajes visibles
+                ],
+              },
+              {
+                featureType: "poi",
+                elementType: "geometry",
+                stylers: [
+                  { visibility: "off" },   // Desactivar los puntos de interés
+                ],
+              },
+            ],
+          }}
+        >
+          {mapLoaded && (
+            <>
+              {currentLocation && (
+                <Marker
+                  position={currentLocation}
+                  icon={{
+                    url: "https://maps.gstatic.com/mapfiles/ms2/micons/man.png",
+                    scaledSize: new window.google.maps.Size(40, 40),
                   }}
+                />
+              )}
+
+              {locations.map((location) => {
+                const icon = getSupermarketIcon(location.name); // Obtener icono si existe
+                return (
+                  icon && (
+                    <Marker
+                      key={location.id}
+                      position={{ lat: location.lat, lng: location.lng }}
+                      icon={{
+                        url: icon,
+                        scaledSize: new window.google.maps.Size(40, 40),
+                      }}
+                      onClick={() => setSelected(location)}
+                    />
+                  )
+                );
+              })}
+
+              {selected && (
+                <InfoWindow
+                  position={{ lat: selected.lat, lng: selected.lng }}
+                  onCloseClick={() => setSelected(null)}
                 >
-                  <img
-                    src={getSupermarketIcon(selected.name)}
-                    alt={selected.name}
+                  <div
                     style={{
-                      width: "50px",
-                      marginBottom: "10px",
-                      borderRadius: "50%",
-                      border: "2px solid #1c802d",
+                      fontFamily: "Arial, sans-serif",
+                      padding: "10px",
+                      textAlign: "center",
+                      backgroundColor: "#fff8e1",
+                      borderRadius: "10px",
+                      boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.2)",
+                      maxWidth: "250px",
                     }}
-                  />
-                  <h3 style={{ margin: "0", fontSize: "16px", color: "#2c3e50" }}>
-                    {selected.name}
-                  </h3>
-                  <p style={{ margin: "5px 0", fontSize: "14px", color: "#34495e" }}>
-                    <strong>Dirección:</strong> {selected.address}
-                  </p>
-                  <p style={{ margin: "5px 0", fontSize: "14px", color: "#34495e" }}>
-                    <strong>Ciudad:</strong> {selected.ciudad}
-                  </p>
-                  <p style={{ margin: "5px 0", fontSize: "14px", color: "#34495e" }}>
-                    <strong>Teléfono:</strong> {selected.telefono}
-                  </p>
-                </div>
-              </InfoWindow>
-            )}
-          </>
-        )}
-      </GoogleMap>
-    </LoadScript>
+                  >
+                    <img
+                      src={getSupermarketIcon(selected.name)}
+                      alt={selected.name}
+                      style={{
+                        width: "50px",
+                        marginBottom: "10px",
+                        borderRadius: "50%",
+                        border: "2px solid #1c802d",
+                      }}
+                    />
+                    <h3 style={{ margin: "0", fontSize: "16px", color: "#2c3e50" }}>
+                      {selected.name}
+                    </h3>
+                    <p style={{ margin: "5px 0", fontSize: "14px", color: "#34495e" }}>
+                      <strong>Dirección:</strong> {selected.address}
+                    </p>
+                    <p style={{ margin: "5px 0", fontSize: "14px", color: "#34495e" }}>
+                      <strong>Ciudad:</strong> {selected.ciudad}
+                    </p>
+                    <p style={{ margin: "5px 0", fontSize: "14px", color: "#34495e" }}>
+                      <strong>Teléfono:</strong> {selected.telefono}
+                    </p>
+                  </div>
+                </InfoWindow>
+              )}
+            </>
+          )}
+        </GoogleMap>
+      </LoadScript>
+    </div>
   );
 };
 
