47971056f267e11d31b879b850358d1d14afd711
Zalmatt <matizalazar2001@gmail.com>
2024-12-11 15:47:37 -0300

    New-Bot de consultas en UserHome

--- vencemio-web/src/pages/userPages/UserHome.js
+++ vencemio-web/src/pages/userPages/UserHome.js
@@ -26,7 +26,31 @@ export default function UserHome() {
 
   const userUid = user?.uid; // Obtener el `uid` desde el contexto de usuario
 
- 
+  // Función para cargar los scripts de Botpress
+  useEffect(() => {
+    const loadBotpressScripts = () => {
+      // Script para el widget de Botpress
+      const script1 = document.createElement("script");
+      script1.src = "https://cdn.botpress.cloud/webchat/v2.2/inject.js";
+      script1.async = true;
+      document.body.appendChild(script1);
+
+      // Script personalizado del bot
+      const script2 = document.createElement("script");
+      script2.src = "https://files.bpcontent.cloud/2024/12/11/17/20241211171207-TCRRU825.js";
+      script2.async = true;
+      document.body.appendChild(script2);
+
+      return () => {
+        // Limpieza al desmontar el componente
+        document.body.removeChild(script1);
+        document.body.removeChild(script2);
+      };
+    };
+
+    loadBotpressScripts();
+  }, []);
+  
   // Función para obtener la distancia entre dos coordenadas
   const calculateDistance = (lat1, lon1, lat2, lon2) => {
     const toRad = (value) => (value * Math.PI) / 180;
